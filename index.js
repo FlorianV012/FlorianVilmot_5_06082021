2035ad66642b2c84a236ba720f4f1c1cd0271e0c
Florian <fvilmot@gmail.com>
2021-08-13 10:16:39 +0200

    Mise en place de l'affichage dynamique de la page "produit"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//recuperation des données de l'api
+//Récupère les données de l'api
 fetch("http://localhost:3000/api/teddies")
   .then(function (res) {
     if (res.ok) {
@@ -6,8 +6,7 @@ fetch("http://localhost:3000/api/teddies")
     }
   })
   .then(function (products) {
-    console.log(products);
-
+    
     //Créer une carte par produit
     for (let i = 0; i < products.length; i++) {
       createCard(products[i].imageUrl, products[i].name, products[i].price, products[i]._id);
@@ -16,20 +15,32 @@ fetch("http://localhost:3000/api/teddies")
   .catch(function (err) {
     // Une erreur est survenue
     console.log(`Erreur : ${err}`);
+  })
+
+  // recupére l'id du produit au clic et l'envoie dans le localStorage
+  .then(function () {
+
+    const card = document.getElementsByClassName("card");
+    for (let i = 0; i < card.length; i++) {
+      card[i].addEventListener('click', (e) => {
+        localStorage.setItem("pageProduct", card[i].id);
+      })
+    }
   });
 
-// fonction de création des cartes de produit
-function createCard(productImageUrl, productName, productPrice,productId) {  
-  document.getElementById("products").innerHTML += 
-  `<div class="col-12 col-md-6 col-lg-4">
+// Fonction de création des cartes de produit
+function createCard(productImageUrl, productName, productPrice, productId) {
+  document.getElementById("products").innerHTML +=
+    `<div class="col-12 col-md-6 col-lg-4">
       <div class="card mx-2 my-4 shadow" id="${productId}">
         <div class="embed-responsive embed-responsive-4by3">
         <img src="${productImageUrl}" alt="" class="card-img-top card-header embed-responsive-item"/>
         </div>
       <div class="card-body">
-        <a href="#" class="text-dark stretched-link card-title h4">${productName}</a>
+        <a href="product.html" class="text-dark stretched-link card-title h4">${productName}</a>
         <p class="card-text">Prix : ${productPrice}€</p>
       </div>
     </div>
   </div>`;
 }
+
--- product.js
+++ product.js
@@ -1,45 +1,45 @@
-fetch("http://localhost:3000/api/teddies/5beaa8bf1c9d440000a57d94")
+//Récupère les données de l'api en fonction de l’ID du produit
+fetch(`http://localhost:3000/api/teddies/${localStorage.pageProduct}`)
   .then(function (res) {
     if (res.ok) {
       return res.json();
     }
   })
   .then(function (product) {
-    createProductPage(product)
-    customisedProduct(product)
+    createProductPage(product);
+    customisedProduct(product);
   })
   .catch(function (err) {
     // Une erreur est survenue
     console.log(`Erreur : ${err}`);
   });
 
-  function createProductPage(product) {  
-        document.getElementById("product-detail").innerHTML += 
-  `<div class="row">
-        <div class="col-5">
-          <img src="${product.imageUrl}" class="rounded img-fluid mt-4" alt="" />
+//Créer le contenu de la page
+function createProductPage(product) {
+  document.getElementById("product-detail").innerHTML += 
+    `<div class="row">
+      <div class="col-5">
+        <img src="${product.imageUrl}" class="rounded img-fluid mt-4" alt="" />
+      </div>
+      <div class="col-7 flex-column mt-4">
+        <div class="m-2">${product.name}</div>
+        <div class="m-2">Prix : ${product.price}€</div>          
+        <div class="col-4 p-0">
+          <select class="form-control" id="customisationOption"></select>
         </div>
-        <div class="col-7 flex-column mt-4">
-          <div class="m-2">${product.name}</div>
-          <div class="m-2">Prix : ${product.price}€</div>
-          
-          <div class="col-4 p-0">
-            <select class="form-control" id="customisationOption">
-              
-            </select>
-          </div>
-
-          <div class="m-2">
-            Description : <br />${product.description}
-          </div>
-          <button type="button" class="btn btn-dark m-2">Ajouter au panier</button>
+        <div class="m-2">
+          Description : <br />${product.description}
         </div>
-      </div>`;
+        <button type="button" class="btn btn-dark m-2">Ajouter au panier</button>
+      </div>
+    </div>`;
 }
 
-
-function customisedProduct(product){
-  for(let i = 0; i < product.colors.length; i++){
-  document.getElementById("customisationOption").innerHTML += `<option>${product.colors[i]}</option>`
-  }  
+// Génère les options de customisation du produit
+function customisedProduct(product) {
+  for (let i = 0; i < product.colors.length; i++) {
+    document.getElementById(
+      "customisationOption"
+    ).innerHTML += `<option>${product.colors[i]}</option>`;
+  }
 }
